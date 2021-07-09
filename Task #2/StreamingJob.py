5adccb85ddf41b7b090d5a3e934ccd10734659ae
justin.van.dongen <justin.van.dongen@munisense.com>
2021-07-09 17:04:03 +0200

    First working streaming version with route data

--- Task #2/StreamingJob.py
+++ Task #2/StreamingJob.py
@@ -1,5 +1,7 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import functions as F
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
@@ -9,7 +11,6 @@ import os
 
 pd.set_option('display.max_columns', 35)
 pd.set_option('display.width', 250000)
-
 
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 
@@ -23,34 +24,41 @@ def createconnection():
 
 
 def createstream(spark):
+    routeschema = StructType().add("index", "string").add("Airline", "string").add("Airline ID", "integer").add(
+        "Source airport",
+        "string").add(
+        "Source airport ID", "integer").add("Destination airport", "string").add("Destination airport ID",
+                                                                                 "integer").add("Codeshare",
+                                                                                                "string").add(
+        "Stops", "integer").add("Equipment", "string")
     lines = spark \
         .readStream \
-        .format("socket") \
-        .option("host", "localhost") \
-        .option("port", 9999) \
-        .load()
+        .option("sep", ",") \
+        .schema(routeschema) \
+        .csv("./tmp/text")
 
-    # Split the lines into words
-    words = lines.select(
-        explode(
-            split(lines.value, " ")
-        ).alias("word")
-    )
+    data = lines.groupBy("Source airport ID").count().orderBy("count", ascending=False).limit(10)
+    print(data)
 
-    # Generate running word count
-    wordCounts = words.groupBy("word").count()
-
-    query = wordCounts \
+    query = data \
         .writeStream \
         .outputMode("complete") \
         .format("console") \
         .start()
 
+    # query = data \
+    #     .writeStream \
+    #     .outputMode("append") \
+    #     .format("csv") \
+    #     .option("path", "output/") \
+    #     .option("checkpointLocation", "checkpoint/") \
+    #     .start()
+
     query.awaitTermination()
 
 
-# def extractdata(spark):
-#     df = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat',
+# def extractdata(spark, url):
+#     df = pd.read_csv(url,
 #                      names=["Airline", "Airline ID", "Source airport", "Source airport ID",
 #                             "Destination airport", "Destination airport ID", "Codeshare", "Stops", "Equipment"])
 #     df = df.replace(np.nan, "", regex=True)
@@ -61,8 +69,8 @@ def createstream(spark):
 #     # print(data)
 #
 #     return data
-#
-#
+
+
 # def analysedata(data):
 #     data = data.groupBy("Source airport ID").count().orderBy("count", ascending=False).limit(10)
 #     data.toPandas().to_csv("sample.csv", header=True)
