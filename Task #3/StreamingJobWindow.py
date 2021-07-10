b70587930f43813d3fe7b3f1cbf0209dbd15e0ab
justin.van.dongen <justin.van.dongen@munisense.com>
2021-07-10 17:07:23 +0200

    Temp commit with both options

--- Task #3/StreamingJobWindow.py
+++ Task #3/StreamingJobWindow.py
@@ -26,6 +26,25 @@ def createconnection():
     return spark
 
 
+def foreach_batch_function(df, epoch_id):
+    windows = []
+    for x in range(0, 68000, 17000):
+        print(x)
+        data = df.where(col('index').between(x, x + 17000))
+        data = data.withColumn("windowNumber", lit(x))
+        data = data.withColumn("window", lit("[" + str(x) + "-" + str(x + 17000) + "]"))
+        data = data.groupBy("Source airport ID", "windowNumber", "window").count().orderBy(
+            col("windowNumber").asc(),
+            col("count").desc()).limit(10)
+        windows.append(data)
+
+    for i in windows[1:]:
+        windows[0] = windows[0].union(i)
+
+    windows[0].show(100)
+
+    pass
+
 def createstream(spark):
     routeschema = StructType().add("index", "integer").add("Airline", "string").add("Airline ID", "integer").add(
         "Source airport",
@@ -40,38 +59,29 @@ def createstream(spark):
         .schema(routeschema) \
         .csv("./tmp/input")
 
-    windows = []
-
-    for x in range(0, 68000, 34000):
-        print(x)
-        data = lines.where(col('index').between(x, x + 34000))
-        data = data.withColumn("windowNumber", lit(x))
-        data = data.withColumn("window", lit("[" + str(x) + "-" + str(x + 34000) + "]"))
-        data = data.groupBy("Source airport ID", "windowNumber", "window").count().orderBy(
-            col("windowNumber").asc(),
-            col("count").desc()).limit(10)
-        windows.append(data)
-        query = data \
-            .writeStream \
-            .outputMode("complete") \
-            .format("console") \
-            .start()
-    query.awaitTermination()
-
-    # for i in windows[1:]:
-    #     windows[0] = windows[0].union(i)
-
-    # output = windows[0].groupBy("Source airport ID", "windowNumber", "window").count().orderBy(col("windowNumber").asc(),
-    #                                                                                        col("count").desc())
-
-    # query = output\
-    #     .writeStream \
-    #     .outputMode("complete") \
-    #     .format("console") \
-    #     .option("numRows", 50) \
-    #     .start()
-
+    # for x in range(0, 68000, 17000):
+    #     print(x)
+    #     data = lines.where(col('index').between(x, x + 17000))
+    #     data = data.withColumn("windowNumber", lit(x))
+    #     data = data.withColumn("window", lit("[" + str(x) + "-" + str(x + 17000) + "]"))
+    #     data = data.groupBy("Source airport ID", "windowNumber", "window").count().orderBy(
+    #         col("windowNumber").asc(),
+    #         col("count").desc()).limit(10)
+    #
+    #     query = data \
+    #         .writeStream \
+    #         .outputMode("complete") \
+    #         .format("console") \
+    #         .start()
+    #
+    # # I do know that they do not close/stop yet, I do not have an idea yet on how to fix that.
     # query.awaitTermination()
+
+    lines.writeStream \
+        .outputMode("append") \
+        .foreachBatch(foreach_batch_function) \
+        .start() \
+        .awaitTermination()
 
 
 if __name__ == '__main__':
